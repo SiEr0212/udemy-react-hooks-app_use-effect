2500ab14e66e7ee2ea40fc00de1d3189e1996102
SiEr0212 <simonertel@gmx.de>
2021-05-02 19:19:47 +0200

    configured useEffects()

--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ let born = false;
 
 function App() {
   const [growth, setGrowth] = useState(0);
+  const [nirvana, setNirvana] = useState(false);
+
+  useEffect(() => {
+    if (born) {
+      document.title = "nirvana attained";
+    }
+  }, [nirvana]);
 
   useEffect(() => {
     console.log("I am born");
-  }, []);//the fist time the compnent is rendered becaus of [] at the end
+  }, []); //the fist time the compnent is rendered becaus of [] at the end
 
   useEffect(() => {
-    born ? console.log("make mistakes and learn"): born = true;
-  });// normally it runs on mounting and every update(no [] at he  end), in this case on every update
+    born ? console.log("make mistakes and learn") : (born = true);
+
+    if (growth > 70) {
+      setNirvana(true);
+    }
+
+   return function cleanup(){
+console.log("cleanup after mistakes");
+   };
+  }); // normally it runs on mounting and every update(no [] at he  end), in this case on every update
 
   const growHandle = () => {
     setGrowth(growth + 10);
