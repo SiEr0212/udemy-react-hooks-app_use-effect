923dc69adcaec59108ed24bc48b507e89429c556
SiEr0212 <simonertel@gmx.de>
2021-05-02 18:09:03 +0200

    adde useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [growth, setGrowth] = useState(0);
+
+  useEffect(() => {
+    console.log("I am born!");
+  }, []);
+
+  const growHandle = () => {
+    setGrowth(growth + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h2>Use Effect</h2>
+        <h3>growth:{growth}</h3>
+        <button onClick={growHandle}>learn and grow</button>
       </header>
     </div>
   );
